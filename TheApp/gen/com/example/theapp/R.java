ffb7c7e79a08b9d185890a0a72073791f9c4ebf5
purezen <aditya2204@gmail.com>
2013-03-06 01:21:44 +0530

    Trying to implement ListView

--- TheApp/gen/com/example/theapp/R.java
+++ TheApp/gen/com/example/theapp/R.java
@@ -16,14 +16,16 @@ public final class R {
     public static final class id {
         public static final int add_task=0x7f070001;
         public static final int listView1=0x7f070004;
-        public static final int menu_settings=0x7f070005;
+        public static final int menu_settings=0x7f070006;
         public static final int task_list=0x7f070003;
         public static final int temp_task_list=0x7f070000;
+        public static final int textView1=0x7f070005;
         public static final int text_add_task=0x7f070002;
     }
     public static final class layout {
         public static final int activity_display_tasks=0x7f030000;
         public static final int activity_main=0x7f030001;
+        public static final int list_item=0x7f030002;
     }
     public static final class menu {
         public static final int activity_display_tasks=0x7f060000;
--- TheApp/src/com/example/theapp/DisplayTasks.java
+++ TheApp/src/com/example/theapp/DisplayTasks.java
@@ -1,9 +1,19 @@
 package com.example.theapp;
 
+import android.annotation.SuppressLint;
+import android.app.ActionBar.LayoutParams;
 import android.app.Activity;
+import android.app.ListActivity;
+import android.app.LoaderManager;
+import android.content.Loader;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.support.v4.content.CursorLoader;
+import android.view.Gravity;
 import android.view.Menu;
+import android.view.ViewGroup;
+import android.widget.ProgressBar;
+import android.widget.SimpleCursorAdapter;
 import android.widget.TextView;
 
 public class DisplayTasks extends Activity {
@@ -15,7 +25,7 @@ public class DisplayTasks extends Activity {
 
 		// Displaying the added task in the list
 		
-		Cursor cursor = new TaskEntry(this).readDb();
+		final Cursor cursor = new TaskEntry(this).readDb();
 		
 		cursor.moveToFirst();
 		
@@ -29,6 +39,58 @@ public class DisplayTasks extends Activity {
 		TextView textView = (TextView)findViewById(R.id.temp_task_list);
 		textView.setTextSize(40);
 		textView.setText(task_summary);
+		
+		@SuppressLint("NewApi")
+		class ListViewLoader extends ListActivity implements LoaderManager.LoaderCallbacks<Cursor>{
+			
+			@Override
+			protected void onCreate(Bundle savedInstanceState) {
+				super.onCreate(savedInstanceState);
+				
+				String fromColumns[] = {TaskEntryDetails.TASK_SUMMARY};
+				int toViews[] = {R.id.textView1};
+				
+				SimpleCursorAdapter adapter;
+				
+				// Create a progress bar to display while the list loads
+				ProgressBar progressBar = new ProgressBar(this);
+				progressBar.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT,
+						LayoutParams.WRAP_CONTENT, Gravity.CENTER));
+				progressBar.setIndeterminate(true);
+				getListView().setEmptyView(progressBar);
+				
+				// Must add the progress bar to the root of the layout
+				ViewGroup root = (ViewGroup) findViewById(android.R.id.content);
+				root.addView(progressBar);
+				
+				adapter = new SimpleCursorAdapter(this, R.id.listView1, cursor, fromColumns, toViews);
+				setListAdapter(adapter);
+				
+				// Prepare the loader.  Either re-connect with an existing one,
+		        // or start a new one.
+		        getLoaderManager().initLoader(0, null, this);
+			}
+
+			@Override
+			public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+				
+				return new ;
+			}
+
+			@Override
+			public void onLoadFinished(Loader<Cursor> arg0, Cursor arg1) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void onLoaderReset(Loader<Cursor> arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+		}
+		
 		
 	}
 
